03d8333eff1661819d22b4d5295652ba986a1ea2
FilippoGalanti <62822036+FilippoGalanti@users.noreply.github.com>
2020-05-31 15:12:17 +0200

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -3,12 +3,12 @@ def bouncy (height, bounces):
     bouncesNumber = 0
     maxHeight = height
     
-    while bouncesNumber <= bounces:
+    while bouncesNumber < bounces:
         
         maxHeight = maxHeight * (0.6)
         bouncesNumber += 1
         print(round(maxHeight,2), 'meters.')
 
-    return 'ended'
+    print('Completed.')
 
 bouncy(100,10)
