2cdb53c8e983f0e6aeeb392ad3c8e21f12e71c62
FilippoGalanti <62822036+FilippoGalanti@users.noreply.github.com>
2020-05-31 16:26:12 +0200

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,21 @@
-# mortgage.py
-#
-# Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+extraPayment = 1000
+extraPaymentStart = 60
+extraPaymentEnd = 108
+
+
+while principal > 0:
+    if months in range (extraPaymentStart, extraPaymentEnd):
+        principal = principal * (1+rate/12) - payment - extraPayment
+        total_paid = total_paid + payment + extraPayment
+        months += 1
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        months += 1      
+
+print('Total paid', total_paid, "in", months, "months.")
